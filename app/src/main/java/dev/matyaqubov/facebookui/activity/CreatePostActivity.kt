8b8c1c6d59ddf6326ded686934f1aeb656933dec
Bogibek_Matyaqubov <uzroot.pdp@gmail.com>
2022-02-22 17:25:16 +0500

    xali o'zgaradi

--- app/src/main/java/dev/matyaqubov/facebookui/activity/CreatePostActivity.kt
+++ app/src/main/java/dev/matyaqubov/facebookui/activity/CreatePostActivity.kt
@@ -1,15 +1,27 @@
 package dev.matyaqubov.facebookui.activity
 
+import android.content.Intent
+import android.graphics.Color
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.TextPaint
+import android.text.style.ClickableSpan
 import android.util.Log
+import android.view.View
+import android.webkit.URLUtil
 import android.widget.*
 import androidx.core.widget.addTextChangedListener
+import com.bumptech.glide.Glide
 import dev.matyaqubov.facebookui.R
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 import org.jsoup.select.Elements
 import java.io.IOException
+import kotlin.math.log
 
 class CreatePostActivity : AppCompatActivity() {
     private lateinit var btn_post: Button
@@ -18,12 +30,14 @@ class CreatePostActivity : AppCompatActivity() {
     private lateinit var tv_title: TextView
     private lateinit var tv_website: TextView
     private lateinit var iv_post: ImageView
+    private lateinit var title: String
+    private lateinit var imgUrl: String
     private lateinit var ll_post: LinearLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_post)
         initviews()
-        gettingData("https://kun.uz/news/2022/01/02/sim-kartalarsiz-apple-raqamli-prokuror-marsda-olingan-yangi-fotosuratlar-hafta-texnologik-dayjesti")
+       // gettingData("https://pdp.uz")
     }
 
     private fun initviews() {
@@ -38,6 +52,7 @@ class CreatePostActivity : AppCompatActivity() {
         et_post.addTextChangedListener {
             btn_post.isEnabled = true
             var post = it.toString()
+            checkURL(post)
 
         }
 
@@ -49,31 +64,44 @@ class CreatePostActivity : AppCompatActivity() {
     }
 
     @Throws(IOException::class)
-    fun gettingData(url:String){
-       Thread(Runnable {
-           val stringBuilder = StringBuilder()
-           try {
-               val doc: Document = Jsoup.connect(url).get()
-               val title: String = doc.title()
-               val links: Elements = doc.select("a[href]")
-               val img=doc.select("img[src~=(?i)\\\\.(png|jpe?g|gif)]")
-               Log.d("dooocc", doc.toString())
-               Log.d("imaggggg", img.toString())
-               stringBuilder.append(title).append("\n")
-               for (link in links) {
-                   stringBuilder.append("\n")
-                       .append("Link :")
-                       .append(link.attr("href")).
-                       append("\n").append("Text : ")
-                       .append(link.text())
-               }
-           } catch (e: IOException) {
-               Log.d("Catch", "gettingData: $e")
-               stringBuilder.append("Error : ").append(e.message).append("\n")
-           }
-           runOnUiThread { tv_title.text = stringBuilder.toString() }
-       }).start()
-    }
+    fun gettingData(url: String) {
+        Thread(Runnable {
+            try {
+                val doc: Document =
+                    Jsoup.connect(url).userAgent("Mozilla/5.0").timeout(10 * 1000).get()
+                title = doc.title()
+                imgUrl = doc.select("meta[property=og:image]").attr("content")
+                Log.d("imgurl", "gettingData: ${imgUrl}")
+                Log.d("docim", "gettingData: ${doc.toString()}")
 
+            } catch (e: IOException) {
+                Log.d("Catch", "gettingData: $e")
+            }
+            runOnUiThread {
+                try {
+                    Glide.with(this).load(imgUrl).into(iv_post)
+                    ll_post.visibility = View.VISIBLE
+                    tv_title.text = title
+                } catch (e:Exception){
+
+                }
+            }
+        }).start()
+    }
+    private fun checkURL(text: String) {
+        val spannable: Spannable = SpannableString(text)
+        val str = text.split(" ").toTypedArray()
+        for (s in str) {
+            if (URLUtil.isValidUrl(s) && s.length>11) {
+                gettingData(s)
+                break
+            } else{
+                ll_post.visibility=View.GONE
+            }
+        }
+//        val textViewUrl = findViewById<TextView>(R.id.tv_link)
+//        textViewUrl.movementMethod = LinkMovementMethod()
+//        textViewUrl.text = spannable
+    }
 
 }
